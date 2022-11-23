660f284536bdc8c801ab1ff58f651f7238df1928
Steve Pepple <stephen.pepple@gmail.com>
2022-11-22 18:45:25 -0800

    Revert panic

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -142,15 +142,14 @@ function apiErrorHandler(error) {
     if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        /*
+        console.log(error.response)
         reporter.panicOnBuild(
             'Request was made, but there was an issue',
             new Error(`Error ${error.response.status} from GravityForms API`)
         )
-        */
 
         //console.log(error.response.data)
-        console.log(error.response.status)
+
         //console.log(error.response.headers)
     } else if (error.request) {
         // The request was made but no response was received
